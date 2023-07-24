eba5e36580b3fa819a46ae8c0e1a5caa8795abc2
Maciej Zagórski <maciejzagorski1996@gmail.com>
2023-07-24 11:16:37 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny"
+});
