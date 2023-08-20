dd2f3fe52d52af16fffb92a6b7d31bf9cf6fbdab
Maciej Zagórski <maciejzagorski1996@gmail.com>
2023-08-20 12:09:52 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName");
 
-button.addEventListener("click", () => {
+
+const hello = () => {
+    console.log("Witam serdecznie wszystkich developerów zaglądających do konsoli")
+}
+hello();
+
+const toggleBackground = () => {
+    const body = document.querySelector(".js-body");
+    const themeName = document.querySelector(".js-themeName");
     body.classList.toggle("dark");
     themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny"
-});
+};
+const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", toggleBackground);
+}
+
+init();
